30d1d9365dd54adab3377af4017ab4ddf1b2a641
Giorgi <Gio.Chiaberashvili@gmail.com>
2023-01-23 15:45:47 +0400

    Completed

--- guessing-game-project.js
+++ guessing-game-project.js
@@ -1,39 +1,67 @@
 const readline = require('readline');
 
-let secretNumber = 7;
-
+secretNumber = randomInRange(0, 100);
+let numAttempts = 3;
 
 const rl = readline.createInterface({
 
     input: process.stdin,
     output: process.stdout
 
-  });
+});
 
-    function askGuess(){
-    rl.question('Enter a guess',function (guess){
-        if(checkGuess(guess)) {
-        console.log("You Win!")
+function askGuess() {
+    if (numAttempts <= 0) {
+        console.log("You Lose")
         rl.close()
-     } else {
-        askGuess();
-     }
-  });
+    } else {
+        rl.question('Enter a guess', function (guess) {
+            if (checkGuess(guess, secretNumber)) {
+                console.log("You Win!")
+                rl.close()
+            } else {
+                numAttempts--;
+                askGuess()
+            }
+        });
+    }
 }
-
-function checkGuess(number){
-    if(number > secretNumber){
+function checkGuess(number) {
+    if (number > secretNumber) {
         console.log("Too High!")
         return false;
     }
-        else if(number < secretNumber){
-            console.log("Too Low!")
-                return false;
-            } else {
-                (number === secretNumber)
-                    console.log("Correct!")
-                    return true;
-                }
-            }
+    if (number < secretNumber) {
+        console.log("Too Low!")
+        return false;
+    } else {
+        console.log("Correct!")
+        return true;
+    }
+}
 
-askGuess(8)
+
+function randomInRange(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+    // The maximum is exclusive and the minimum is inclusive
+}
+function askLimit() {
+    rl.question("Enter the number of attempts: ", function (attempts) {
+        numAttempts = attempts;
+        askRange();
+    });
+}
+
+function askRange() {
+    rl.question("Enter a maximum number: ", function (max) {
+        rl.question("Enter a minumum number: ", function (min) {
+            secretNumber = randomInRange(min, max);
+            // askLimit();
+            askGuess();
+        });
+    });
+}
+
+askLimit()
